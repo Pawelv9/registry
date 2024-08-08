3a1961ce4a409667c30d9b4e7d2022d3157bee1d
Pavel <pawelv9@gmail.com>
2024-08-08 22:12:36 +0300

    React Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: "/search",
+                element: <SearchPage />,
+            },
+            {
+                path: "/packages/:name",
+                element: <DetailsPage />,
+            },
+        ],
+    },
+]);
 
 function App() {
-  return (
-    <>
-        <div className="m-10 p-4 flex items-center justify-between">
-            Hi there
-        </div>
-    </>
-  )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
