0a95dc921a30ebe3e145e33e51a73a23f32ef69d
Pavel <pawelv9@gmail.com>
2024-08-08 16:43:46 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Source Code: https://www.udemy.com/course/react-redux/
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+        <div className="m-10 p-4 flex items-center justify-between">
+            Hi there
+        </div>
+    </>
+  )
+}
+
+export default App
