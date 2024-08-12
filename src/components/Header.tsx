c179eccdeab5d132e94828164dcd084416214553
Pavel <pawelv9@gmail.com>
2024-08-12 21:21:58 +0300

    added styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,13 @@ import SearchInput from "./SearchInput.tsx";
 
 function Header() {
     return (
-        <div>
-            <Link to="/">NPM Registry</Link>
-            <SearchInput />
+        <div className='flex items-center justify-between px-4 border-b h-14'>
+            <div className='flex items-center space-x-2 text-sm'>
+                <Link className='text-lg fonst-bold' to="/">NPM Registry</Link>
+            </div>
+            <div className='w-full max-xl ml-4'>
+                <SearchInput />
+            </div>
         </div>
     );
 }
