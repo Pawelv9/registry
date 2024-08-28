c8901219cba882c4f54316bcb1b7ba766e7ce548
Pavel <pawelv9@gmail.com>
2024-08-28 12:37:31 +0300

    rendering Package Details

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -5,7 +5,27 @@ function DetailsPage() {
     const { details } = useLoaderData() as DetailsLoaderResult;
 
     return (
-        <div>{details.name} - {details.description}</div>
+        <div className='space-y-4'>
+            <h1 className='text-3xl font-bold my-4'>
+                {details.name}
+            </h1>
+            <div>
+                <h3 className='text-lg font-bold'>
+                    Description
+                </h3>
+                <div className='p-3 bg-gray-200 rounded'>
+                    {details.description}
+                </div>
+            </div>
+            <div>
+                <h3 className='text-lg font-bold'>
+                    License
+                </h3>
+                <div className='p-3 bg-gray-200 rounded'>
+                    {details.license}
+                </div>
+            </div>
+        </div>
     );
 }
 
